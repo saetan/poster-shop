e480706097954f4e30d725613d1527ed12e971c4
saetan <saetan@live.com>
2021-03-25 22:29:42 +0800

    25_03 Update

--- public/script.js
+++ public/script.js
@@ -1,10 +1,16 @@
+var loadNum = 4;
+var watcher;
+
 new Vue({
     el: '#app',
     data: {
         total:0,
         products: [],
         cart: [],
-        search: "",
+        search: "cat",
+        lastSearch: "",
+        loading: false,
+        results: [],
     },
     methods: {
         addToCart: function(product){ //every time this method is being called
@@ -42,16 +48,42 @@ new Vue({
         },
 
         onSubmit: function(){
+            this.products =[]; //make an empty page when press search to give user know that it's loading
+            this.loading = true;
             var path = "/search?q=".concat(this.search);
-            this.$http.get(path).then(function(response) {
-                console.log("uwu"); //new changes
-            }); //$ sign indiicatiion is a plugin method
+            this.$http.get(path).then(function(response) { //$ sign indiicatiion is a plugin method
+                this.results = response.body;
+                this.lastSearch = this.search;
+                this.appendResults();
+                this.loading = false;
+            }); 
+        },
+        appendResults: function(){
+            if(this.products.length < this.results.length) {
+                var toAppend = this.results.slice(this.products.length, loadNum + this.products.length); //putting response as products, we slice it to show 4 items at one, scroll loading
+                this.products = this.products.concat(toAppend);
+            }
         }
-
     },
     filters:{
         currency: function(price){
             return "$".concat(price.toFixed(2)); //fixing price to 2 decimal place and concatenate $ infront of each price
+        }
+    },
+    created: function() {
+        this.onSubmit();
+    },
+
+    updated: function() {
+        var sensor = document.querySelector("#product-list-bottom");
+        watcher = scrollMonitor.create(sensor);
+        watcher.enterViewport(this.appendResults); 
+    },
+
+    beforeUpdate: function(){
+        if(watcher){
+            watcher.destroy();
+            watcher = null;
         }
     }
 });
